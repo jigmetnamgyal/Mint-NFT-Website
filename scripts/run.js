cbf7ab138edb891e506c8fb35a74dc4d8e932285
JigmeT123 <tashinamgyaljigme@gmail.com>
2022-03-13 15:57:09 +0600

    Refactored(): spacing

--- scripts/run.js
+++ scripts/run.js
@@ -1,18 +1,18 @@
 const main = async () => {
-    const nftContractFactory = await hre.ethers.getContractFactory('PomNFT');
-    const nftContract = await nftContractFactory.deploy();
-    await nftContract.deployed();
-    console.log('Contract deployed to:', nftContract.address);
+	const nftContractFactory = await hre.ethers.getContractFactory("PomNFT");
+	const nftContract = await nftContractFactory.deploy();
+	await nftContract.deployed();
+	console.log("Contract deployed to:", nftContract.address);
 };
 
 const runMain = async () => {
-    try {
-        await main();
-        process.exit(0)
-    }catch (error) {
-        console.log(error);
-        process.exit(1)
-    }
-}
+	try {
+		await main();
+		process.exit(0);
+	} catch (error) {
+		console.log(error);g
+		process.exit(1);
+	}
+};
 
-runMain()
+runMain();
