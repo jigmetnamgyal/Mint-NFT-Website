cde68414262e79df135d2481684004a601520827
JigmeT123 <tashinamgyaljigme@gmail.com>
2022-03-13 16:01:54 +0600

    Imported(): required openzeppelin packages

--- contracts/PomNFT.sol
+++ contracts/PomNFT.sol
@@ -2,6 +2,9 @@
 
 pragma solidity ^0.8.1;
 
+// We first import some OpenZeppelin Contracts.
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
 contract PomNFT {
