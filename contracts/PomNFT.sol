51ea8afdf37cfe5bc8c30609d6e845ab2276fcd8
JigmeT123 <tashinamgyaljigme@gmail.com>
2022-03-13 17:33:45 +0600

    Added(): functions to randomize svg

--- contracts/PomNFT.sol
+++ contracts/PomNFT.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.1;
 
 // We first import some OpenZeppelin Contracts.
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
@@ -14,9 +15,75 @@ contract PomNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
+    string baseSvg =
+        "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: lightgreen; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
+    // I create three arrays, each with their own theme of random words.
+    // Pick some random funny words, names of anime characters, foods you like, whatever!
+    string[] firstWords = [
+        "Bhutan",
+        "Naruto",
+        "Tanjiro",
+        "Photosynthesis",
+        "Luffy",
+        "Hello"
+    ];
+    string[] secondWords = [
+        "World",
+        "Awesome",
+        "Pistol",
+        "Japan",
+        "Saw",
+        "See"
+    ];
+    string[] thirdWords = ["Pencil", "BMW", "Lambo", "Zenitsu", "Word", "Coke"];
+
     // We need to pass the name of our NFTs token and its symbol.
     constructor() ERC721("Persona of the Metaverse", "POM") {
         console.log("This is my NFT contract. Woah!");
+    }
+
+    // I create a function to randomly pick a word from each array.
+    function pickRandomFirstWord(uint256 tokenId)
+        public
+        view
+        returns (string memory)
+    {
+        // I seed the random generator. More on this in the lesson.
+        uint256 rand = random(
+            string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId)))
+        );
+        // Squash the # between 0 and the length of the array to avoid going out of bounds.
+        rand = rand % firstWords.length;
+        return firstWords[rand];
+    }
+
+    function pickRandomSecondWord(uint256 tokenId)
+        public
+        view
+        returns (string memory)
+    {
+        uint256 rand = random(
+            string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId)))
+        );
+        rand = rand % secondWords.length;
+        return secondWords[rand];
+    }
+
+    function pickRandomThirdWord(uint256 tokenId)
+        public
+        view
+        returns (string memory)
+    {
+        uint256 rand = random(
+            string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId)))
+        );
+        rand = rand % thirdWords.length;
+        return thirdWords[rand];
+    }
+
+    function random(string memory input) internal pure returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(input)));
     }
 
     // A function our user will hit to get their NFT.
@@ -24,8 +91,22 @@ contract PomNFT is ERC721URIStorage {
         // Get the current tokenId, this starts at 0.
         uint256 newItemId = _tokenIds.current();
 
+        string memory first = pickRandomFirstWord(newItemId);
+        string memory second = pickRandomSecondWord(newItemId);
+        string memory third = pickRandomThirdWord(newItemId);
+
+        string memory finalSvg = string(
+            abi.encodePacked(baseSvg, first, second, third, "</text></svg>")
+        );
+
+        console.log("\n--------------------");
+        console.log(finalSvg);
+        console.log("--------------------\n");
+
         // Actually mint the NFT to the sender using msg.sender.
         _safeMint(msg.sender, newItemId);
+
+        // I concatenate it all together, and then close the <text> and <svg> tags.
 
         // Set the NFTs data.
         _setTokenURI(
